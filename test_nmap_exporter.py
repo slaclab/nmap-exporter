5514be872a392d43cf0caa773aa64d4444d6ecb9
Micha Okun <mokun@slac.stanford.edu>
2025-08-10 02:46:22 -0700

    add blackbox example

--- test_nmap_exporter.py
+++ test_nmap_exporter.py
@@ -4,3 +4,8 @@ import nmap_exporter as ne
 """
 NMAP_COLLECTOR_IP_RANGE="google.com totally.bogus.nonexistentdomain5273.foobar s3df.slac.stanford.edu grafana.slac.stanford.edu influxdb.slac.stanford.edu prometheus.slac.stanford.edu k8s.slac.stanford.edu sdfrepo.sdf.slac.stanford.edu sdfregistry001.slac.stanford.edu coact.slac.stanford.edu sdfloki.slac.stanford.edu" NMAP_COLLECTOR_SCAN_METHOD="-sT -T4 -p443 --script ssl-cert --max-parallelism=1000 --script-timeout=10 --host-timeout=15" docker run -e NMAP_COLLECTOR_IP_RANGE -e NMAP_COLLECTOR_SCAN_METHOD -p 8000:8000 nmap-exporter:test
 """
+
+# hthttp://localhost:9115/probe?target=google.com&module=http_2xx tps://github.com/prometheus/blackbox_exporter?tab=readme-ov-file
+"""
+docker run --rm   -p 9115:9115   --name blackbox_exporter   -v $(pwd):/config   quay.io/prometheus/blackbox-exporter:latest 
+"""
